26b5686ffe20c1a32aef63f2bd5f1ca3d460a0b0
Multichiki22 <myguelangel12@gmail.com>
2023-08-20 02:54:05 -0500

    Change in the default port

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3000
 const cors = require("cors");
 const morgan = require('morgan');
 const corsOptions = require("./config/corsOptions");
