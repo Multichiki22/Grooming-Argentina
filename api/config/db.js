9068b66481a6a92a9d8f7d90cf94e6103f2eb48a
Multichiki22 <myguelangel12@gmail.com>
2023-08-20 02:53:20 -0500

    Small change in the db connection

--- api/config/db.js
+++ api/config/db.js
@@ -1,24 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectionString = process.env.MONGO_DB_URI;
 
 mongoose.set("strictQuery", false);
 
 module.exports = () => {
-  const connect = () => {
+  const connect = async () => {
     try {
-      mongoose.connect(
-        connectionString,
-        {
-          keepAlive: true,
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        },
-      );
+      mongoose.connect(connectionString, {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("execution of connection");
+     
     } catch (error) {
       console.log(error);
-    };
+    }
   };
   connect();
 };
